df1fed098ca924d591070399e9f38d21b859d0f5
Angela Wang <ea103t938@gmail.com>
2020-09-26 14:54:33 +0800

    cat version

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-		private double deffense;
+		private float deffense;
 
         System.out.println("Hello World!");
     }
